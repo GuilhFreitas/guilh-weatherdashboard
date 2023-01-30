b8e5848edc36cf1c07b60e151a11468a46a96937
GuillhFreitas <guilherme.antraco@gmail.com>
2023-01-30 19:03:59 +0000

    created two new functions displayHistory and storeHistory. Search history is now displayed when page is opened.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,23 @@ let searchButtonEl = document.querySelector('#search-button');
 let searchInputEl = document.querySelector('#search-input');
 let asideEl = document.querySelector('aside')
 
+displayHistory();
+
 asideEl.addEventListener('click', function(event){
     event.preventDefault();
 
     let cityName = '';
+    // distinguishes between search button and search history
     if (event.target.matches('#search-button')){
         cityName = searchInputEl.value;
+        storeHistory(cityName);
     }else if(event.target.matches('.btn-history')){
         cityName = event.target.textContent;
     }else{
         return;
     }
 
+    // fetches latitude and longitude for the search term using Geocoding API
     let APIkey = '0fa3bf96a49d09b12dcec88d6fd1e0bd';
     let geoQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIkey}`
     let lat = '';
@@ -30,6 +35,8 @@ asideEl.addEventListener('click', function(event){
 
         return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`) 
     })
+
+    // fetches current weather data and displays it on the page
     .then(response => response.json())
     .then(function(weatherData){
         console.log(weatherData);
@@ -44,13 +51,12 @@ asideEl.addEventListener('click', function(event){
         <p>Temp: ${temp} °C</p>
         <p>Wind: ${wind} KPH</p>
         <p>Humidity: ${humidity}%`;
+    })
+})
 
-        let buttonEl = document.createElement('button');
-        buttonEl.setAttribute('class', 'btn btn-secondary btn-history');
-        buttonEl.textContent = cityName;
-        historyEl.prepend(buttonEl);
-
-        let searchHistory = JSON.parse(localStorage.getItem('search_history'));
+// saves searched city to history in local storage and refreshes displayed history
+function storeHistory(cityName){
+    let searchHistory = JSON.parse(localStorage.getItem('search_history'));
         if (searchHistory !== null){
             if(searchHistory.length >= 6){
                 searchHistory.pop();
@@ -61,5 +67,19 @@ asideEl.addEventListener('click', function(event){
             localStorage.setItem('search_history', JSON.stringify([cityName]));
         }
         console.log(JSON.parse(localStorage.getItem('search_history')));
-    })
-})
+        displayHistory();
+}
+
+// displays search history on the page
+function displayHistory(){
+    let searchHistory = JSON.parse(localStorage.getItem('search_history'));
+    historyEl.innerHTML = '';
+    for (let i = 0; i < searchHistory.length; i++) {
+        const city = searchHistory[i];
+        
+        let buttonEl = document.createElement('button');
+        buttonEl.setAttribute('class', 'btn btn-secondary btn-history');
+        buttonEl.textContent = city;
+        historyEl.prepend(buttonEl);
+    }
+}
